09947f7606198901c74786588366d2b7ffdb0761
AJ Funk <aj.funk@onmego.com>
2015-11-16 15:13:42 -0800

    allow users to add games

--- client/app/games/games.controller.js
+++ client/app/games/games.controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('meanTutorialApp')
-  .controller('GamesCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('GamesCtrl', function ($scope, $http) {
+
+    $http.get('/api/games')
+    .success(function(data) {
+      $scope.games = data;
+      console.log($scope.games);
+    })
+    .error(function(err) {
+      alert('Error! Something went wrong');
+    });
+
+    $scope.addNewGame = function(){
+      $http.post('/api/games', $scope.newGame)
+      .success(function(){
+        $scope.games.push($scope.newGame);
+        $scope.newGame = {};
+      })
+      .error(function(err){
+        alert('Error! Something went wrong');
+      });
+    };
+
   });
