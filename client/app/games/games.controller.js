f3895ed9d352baffaccf74f68c98101de686b7a3
AJ Funk <aj.funk@onmego.com>
2015-11-16 22:02:39 -0800

    allow users to delete and edit games

--- client/app/games/games.controller.js
+++ client/app/games/games.controller.js
@@ -6,7 +6,6 @@ angular.module('meanTutorialApp')
     $http.get('/api/games')
     .success(function(data) {
       $scope.games = data;
-      console.log($scope.games);
     })
     .error(function(err) {
       alert('Error! Something went wrong');
@@ -17,6 +16,30 @@ angular.module('meanTutorialApp')
       .success(function(){
         $scope.games.push($scope.newGame);
         $scope.newGame = {};
+      })
+      .error(function(err){
+        alert('Error! Something went wrong');
+      });
+    };
+
+    $scope.deleteGame = function(index){
+      $http.delete('/api/games/' + $scope.games[index]._id)
+      .success(function(){
+        $scope.games.splice(index, 1);
+      })
+      .error(function(err){
+        alert('Error! Something went wrong');
+      });
+    };
+
+    $scope.toggleEdit = function(index){
+      $scope.games[index].edit = !$scope.games[index].edit;
+    };
+
+    $scope.saveGame = function(index){
+      $http.put('/api/games/' + $scope.games[index]._id, $scope.games[index])
+      .success(function(){
+        $scope.games[index].edit = false;
       })
       .error(function(err){
         alert('Error! Something went wrong');
